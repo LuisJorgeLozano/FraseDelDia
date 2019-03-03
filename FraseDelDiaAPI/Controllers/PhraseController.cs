5f38330add4cb419f5de59b0c13b4b194d5bf801
LuisJorgeLozano <luisjorge_lozano@hotmail.com>
2019-03-03 17:51:07 -0600

    Primera Versión

---
+++ FraseDelDiaAPI/Controllers/PhraseController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+
+namespace FraseDelDiaAPI.Controllers
+{
+    public class Phrase
+    {
+        public string phrase;
+        public string author;
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhraseController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<string> GetAsync()
+        {
+            // Getting the HTML
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("http://www.frasedehoy.com");
+            var pageContents = await response.Content.ReadAsStringAsync();
+
+            // Extracting Content
+            HtmlDocument pageDocument = new HtmlDocument();
+            pageDocument.LoadHtml(pageContents);
+
+            string statment = pageDocument.DocumentNode.SelectSingleNode("(//div[contains(@id,'frase_container')]//h1)[1]").InnerText;
+            statment = statment.Replace("&quot;", string.Empty);
+
+            string author = pageDocument.DocumentNode.SelectSingleNode("(//div[contains(@id,'frase_container')]//p)[1]").InnerText;
+            author = author.Replace("&mdash;", string.Empty);
+
+            Phrase resultantPhrase = new Phrase
+            {
+                phrase = statment,
+                author = author
+            };
+
+            return JsonConvert.SerializeObject(resultantPhrase);
+        }
+    }
+}
